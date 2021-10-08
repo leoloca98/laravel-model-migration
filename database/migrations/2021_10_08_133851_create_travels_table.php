1c879837214c2c348acea8f26244c362ae4978e7
leoloca98 <leonardolocatelli98@gmail.com>
2021-10-08 15:50:05 +0200

    Create_Travels_Table

---
+++ database/migrations/2021_10_08_133851_create_travels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->float('price', 10, 2)->unsigned(); // Numero con massimo 10 cifre (comprese le 2 decimali) che non può avere segno
+            $table->smallInteger('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
